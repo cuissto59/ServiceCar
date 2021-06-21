10230ea1639f087186ceea45877f7d9e4e4441c9
Abdellah-Hmade <Abdellah-Hmade>
2021-06-21 11:04:59 +0100

    changes in conductor

--- Controllers/ConductorController.cs
+++ Controllers/ConductorController.cs
@@ -25,8 +25,8 @@ namespace serviceCar.Controllers
             return View(await servicecarContext.ToListAsync());
         }
 
-        // GET: Conductor/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Conductor/Profil/1
+        public async Task<IActionResult> Profil(int? id)
         {
             if (id == null)
             {
@@ -44,29 +44,10 @@ namespace serviceCar.Controllers
             return View(conductor);
         }
 
-        // GET: Conductor/Create
-        public IActionResult Create()
-        {
-            ViewData["User"] = new SelectList(_context.User, "IdUser", "FName");
-            return View();
-        }
 
         // POST: Conductor/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("User,Cin,TelephoneNumber,Adress,Active")] Conductor conductor)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(conductor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["User"] = new SelectList(_context.User, "IdUser", "FName", conductor.User);
-            return View(conductor);
-        }
 
         // GET: Conductor/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -119,36 +100,6 @@ namespace serviceCar.Controllers
             }
             ViewData["User"] = new SelectList(_context.User, "IdUser", "FName", conductor.User);
             return View(conductor);
-        }
-
-        // GET: Conductor/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var conductor = await _context.Conductor
-                .Include(c => c.UserNavigation)
-                .FirstOrDefaultAsync(m => m.User == id);
-            if (conductor == null)
-            {
-                return NotFound();
-            }
-
-            return View(conductor);
-        }
-
-        // POST: Conductor/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var conductor = await _context.Conductor.FindAsync(id);
-            _context.Conductor.Remove(conductor);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ConductorExists(int id)
